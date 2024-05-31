eee5ba59f18ff1c2c61081482c637c7fa0f0e1e7
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 07:24:04 +0700

    Setup CICD :four:🫸🌀✏️📗🐧🐳⬆

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #aws ecr get-login-password | docker login --username AWS --password-stdin "${AWS_ECR_REGISTRY}"
 ssh -p "${SERVER_PORT}" "${SERVER_USERNAME}"@"${SERVER_HOST}" -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+cd /home/deployer/coffee-shop
+touch .env
+echo AWS_REGION=${{ secrets.AWS_REGION }} >> .env
+echo AWS_ECR_REGISTRY=${{ secrets.AWS_ECR_REGISTRY }} >> .env
+echo IMAGE_TAG=${{secrets.IMAGE_TAG}}
+cat .env
 start=$(date +"%s")
 aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${AWS_ECR_REGISTRY}"
-#ssh "${SERVER_USERNAME}"@"${SERVER_HOST}":"${SERVER_PORT}" -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
-docker pull ${AWS_ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+docker pull 267023985114.dkr.ecr.ap-southeast-1.amazonaws.com/coffee-shop:${IMAGE_TAG}
 
-CONTAINER_NAME=mhdc-main
+CONTAINER_NAME=${ECR_REPOSITORY}
 if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
     if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
         echo "Container is running -> stopping it..."
