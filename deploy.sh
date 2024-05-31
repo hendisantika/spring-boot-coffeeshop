5f6dd66a1a28a8a8d24e6152d9ca134d99983902
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-05-31 15:01:32 +0000

    improve CI/CD 2

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,10 @@
 #aws ecr get-login-password | docker login --username AWS --password-stdin "${AWS_ECR_REGISTRY}"
 ssh -p "${SERVER_PORT}" "${SERVER_USERNAME}"@"${SERVER_HOST}" -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
 cd /home/deployer/coffee-shop
-touch .env
-echo AWS_REGION=$AWS_REGION >> .env
-echo HENDI=HENDI >> .env
-echo AWS_ECR_REGISTRY=$AWS_ECR_REGISTRY >> .env
-echo IMAGE_TAG=$IMAGE_TAG
 cat .env
+set +a
+source .env
+set -a
 start=$(date +"%s")
 aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${AWS_ECR_REGISTRY}"
 docker pull 267023985114.dkr.ecr.ap-southeast-1.amazonaws.com/coffee-shop:${IMAGE_TAG}
